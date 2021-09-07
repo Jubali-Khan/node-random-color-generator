03fcd6f1f3c1e03312f96df10902d0dfa30d4bcb
Jubran Naser <jubran.naser@protonmail.com>
2021-09-07 23:09:06 +0200

    Delete unnecessary files

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const clc = require('cli-color');
 
-console.log(clc.red('Text in red'));
+// if (process.argv[2]) {
+//   if (process.argv[2] === 'ask') {
+//     // ask outcome
+//   } else if (process.argv[2] !== 'ask') {
+//     if (process.argv[3]) {
+//       // 2 inputs outcome
+//     } else {
+//       // 1 input outcome
+//       let color = process.argv[2];
+//       switch (color) {
+//         case 'red':
+//           console.log(clc.red('text'));
+//           break;
+//         default:
+//           console.log('please enter one of the 10 hues available next time');
+//       }
+//     }
+//   }
+// } else {
+//   // random color outcome
+//   // const randomColor = clc.xterm(Math.floor(Math.random() * 4));
+//   // console.log(randomColor('text')); // why does it work here and not throw randomColor is not a function??
 
-if (process.argv[2] !== '') {
-  if (process.argv[2] !== 'ask') {
-    if (process.argv[3]) {
-      // 2 inputs outcome
-    } else {
-      // 1 input outcome
-    }
-  } else {
-    // ask outcome
-  }
-} else {
-  // random color outcome
+// }
+console.log(Math.floor(Math.random() * 4));
+
+function between(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
+
+console.log(between(0, 4));
