a35a94052ff935ae0ff78da646c2a7e1b720e4b4
Jubran Naser <jubran.naser@protonmail.com>
2021-09-11 23:23:24 +0200

    Create function colorify color output

--- trial.js
+++ trial.js
@@ -12,13 +12,12 @@ const randomLuminosity = basicLuminosities[between(0, 2)];
 
 function colorify(par1, par2) {
   let tempColor = par2 + par1;
+  // const theHue = par1; // User input? No, user input converted
   let theHex;
   let colorText;
 
   if (par1 && par2) {
-    // two input outcome
     if (basicHues.indexOf(par1) > -1 && basicLuminosities.indexOf(par2) > -1) {
-      // light red
       if (par1 === 'red' && par2 === 'light') {
         // couldn't find lightred in css colors so used indianred instead
         theHex = toHex('indianred');
@@ -33,7 +32,6 @@ function colorify(par1, par2) {
 ###############################`;
         console.log(theHex);
         console.log(chalk.keyword('indianred')(colorText));
-        // other color-luminosity combinations
       } else if (basicHues.indexOf(par1) > -1) {
         tempColor = par2 + par1;
         theHex = toHex(tempColor);
@@ -76,6 +74,8 @@ function colorify(par1, par2) {
 
 const para1 = process.argv[2];
 const para2 = process.argv[3];
+// colorify(para1, para2);
+// colorify(randomHue, randomLuminosity);
 
 if (!process.argv[2]) {
   colorify(randomHue, randomLuminosity);
